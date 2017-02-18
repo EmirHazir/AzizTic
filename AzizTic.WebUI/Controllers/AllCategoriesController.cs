e5120bf95f2cf56b945f2a3391a46ac9f920c400
EmirHazir <emircanaziz@gmail.com>
2017-02-18 06:26:12 +0300

    kategori sayfasını giydirdim

---
+++ AzizTic.WebUI/Controllers/AllCategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using AzizTic.Services.Abstract;
+using AzizTic.WebUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzizTic.WebUI.Controllers
+{
+    public class AllCategoriesController : Controller
+    {
+        //private IKategoriService _kategoriService;
+        //public AllCategoriesController(IKategoriService kategoriService)
+        //{
+        //    _kategoriService = kategoriService;
+        //}
+        public ActionResult Index()
+        {
+            //KategoriVM model = new KategoriVM
+            //{
+            //    Kategorilers = _kategoriService.GetAll()
+            //};
+            return View();//modeli basıcam ama önyüzde patlıyor 
+        }
+    }
+}
--- AzizTic.WebUI/Controllers/HomeController.cs
+++ AzizTic.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AzizTic.Services.Abstract;
+using AzizTic.WebUI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
